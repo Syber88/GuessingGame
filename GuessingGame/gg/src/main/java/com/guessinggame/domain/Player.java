bf7a142f24e5235227698f58f63e8871c02b962c
Syber88 <ssmagwaza88@gmail.com>
2025-11-01 15:34:43 +0200

    added Player class in order to enable game stats saving

---
+++ GuessingGame/gg/src/main/java/com/guessinggame/domain/Player.java
@@ -0,0 +1,43 @@
+package com.guessinggame.domain;
+
+public class Player {
+    private String name;
+    private int wins;
+    private int losses;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    
+
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof Player)){
+            return false;
+        }
+
+        Player objPlayer = (Player) obj;
+        if (this.name == objPlayer.name){
+            return true;
+        }
+        return false;
+    }
+
+    
+    
+}
