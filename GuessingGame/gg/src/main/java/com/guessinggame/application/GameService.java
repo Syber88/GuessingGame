0b74f84cd4491718c2094eda045e3dec77f94ab0
Syber88 <ssmagwaza88@gmail.com>
2025-11-15 18:48:45 +0200

    done with score class and tests

--- GuessingGame/gg/src/main/java/com/guessinggame/application/GameService.java
+++ GuessingGame/gg/src/main/java/com/guessinggame/application/GameService.java
@@ -13,20 +13,20 @@ public class GameService
         ConsoleUI ui = new ConsoleUI(scanner);
         PlayerScoreHistory history = new PlayerScoreHistory();
 
-        String name = ui.askForName();
-        Player player = new Player(name);
+        // String name = ui.askForName();
+        // Player player = new Player(name);
         String fileName = history.getFileName();
 
-        if (history.playerExists(name, fileName)){
-            ui.showMessage("Welcome back, Old friend");
-            player = history.loadPlayerStats(player, fileName);
-        } else {
-            ui.showMessage("Greetings " + player.getName() + "\n");
+        // if (history.playerExists(name, fileName)){
+            // ui.showMessage("Welcome back, Old friend");
+            // player = history.loadPlayerStats(player, fileName);
+        // } else {
+            // ui.showMessage("Greetings " + player.getName() + "\n");
         }
         
-        Game game = new Game(scanner);
-        history.showAllTimePlayerStats(player, ui);
-        game.start();
-        history.saveStats(player, fileName, game);
+        // Game game = new Game(scanner);
+        // history.showAllTimePlayerStats(player, ui);
+        // game.start();
+        // history.saveStats(player, fileName, game);
     }
 }
