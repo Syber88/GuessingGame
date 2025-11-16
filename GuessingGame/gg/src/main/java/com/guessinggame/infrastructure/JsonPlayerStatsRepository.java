471e9408c399e9b81a0644ec8d63ec1533fa6efb
Syber88 <ssmagwaza88@gmail.com>
2025-11-16 17:11:28 +0200

    partially done with logic for saving to a file

--- GuessingGame/gg/src/main/java/com/guessinggame/infrastructure/JsonPlayerStatsRepository.java
+++ GuessingGame/gg/src/main/java/com/guessinggame/infrastructure/JsonPlayerStatsRepository.java
@@ -1,24 +1,82 @@
 package com.guessinggame.infrastructure;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guessinggame.domain.*;;
 
 public class JsonPlayerStatsRepository implements PlayerStatsRepository{
+    private final String filename;
+
+    public JsonPlayerStatsRepository(){
+        this.filename = "GuessingGame/gg/src/PlayerScoreHistory.json";
+    }
 
     @Override
     public Player load(String name) {
-        Map<String, Map<String, Integer>> data = readPlayerHistoryFile(fileName);
-        Map<String,Integer>playerMap = data.get(name);
-        player.setLosses(playerMap.get("losses"));
-        player.setWins(playerMap.get("wins"));
-        return player;
+        try {
+            Map<String, Map<String, Integer>> data = readFile(filename);
+            Map<String,Integer>playerMap = data.get(name);
+            Player player = new Player(name, new ScoreTracker());
+            int losses = playerMap.getOrDefault("losses",0);
+            int wins = playerMap.get("wins");
+            player.restoreScores(wins, losses);
+            return player;
+
+        } catch (IOException e){
+            System.out.println("Error opening file: " + e.getMessage());
+            return new Player(name, new ScoreTracker());
+        }
     }
 
     @Override
     public void save(Player player) {
-        // TODO Auto-generated method stub
+        try {
+            Map<String, Map<String, Integer>> data = readFile(filename);
+            Map<String, Integer> playerMap = data.computeIfAbsent(player.getName(), k -> {
+                Map<String, Integer> pm = new HashMap<>();
+                pm.put("wins",0);
+                pm.put("losses", 0);
+                return pm;
+            });
+    
+            int wins = playerMap.getOrDefault("wins",0), losses = playerMap.getOrDefault("losses",0); 
+            wins += player.getWins();
+            losses += player.getLosses();
+    
+            playerMap.put("wins", wins);
+            playerMap.put("losses", losses);
+    
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filename), data);
+
+        } catch (IOException e){
+            System.out.println("Error opening file: " + e.getMessage());
+        }
         
+    }
+
+    public Map<String, Map<String, Integer>> readFile(String fileName) throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File(fileName);
+        if (file.length() == 0){
+            return new HashMap<>();
+        }
+        
+        Map<String, Map<String, Integer>> data = mapper.readValue(file, 
+        new com.fasterxml.jackson.core.type.TypeReference<Map<String, Map<String, Integer>>>() {});
+        return data;
+    }
+
+    public boolean playerExists(String name, String fileName) throws IOException{
+        Map<String, Map<String, Integer>> data = readFile(fileName);
+        if (data.containsKey(name)){
+            return true;
+        }
+        return false;
     }
 
 }
